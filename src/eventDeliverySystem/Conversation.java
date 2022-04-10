d909caaf072e05a3345f94cffb46bb2e75b135db
user <user@DESKTOP-S968RHA>
2022-04-10 16:28:45 +0300

    Complete interface and doc

--- src/eventDeliverySystem/Conversation.java
+++ src/eventDeliverySystem/Conversation.java
@@ -1,34 +1,62 @@
 package eventDeliverySystem;
 
 import java.util.Stack;
+import java.util.concurrent.ThreadLocalRandom;
 
+/**
+ * A data structure holding the contents of a conversation/topic between users.
+ * Note that it does NOT keep track of users subscribed to it.
+ * @author user
+ *
+ */
 class Conversation {
 	private final String name;
-	private final int id;
+	private final long id;
 	private final Stack<RawData> posts;
 	
+	/**
+	 * Create a new conversation.
+	 * @param name the name displayed when viewing the conversation.
+	 * @param id
+	 */
 	public Conversation(String name, int id) {
 		super();
 		this.name = name;
-		this.id = id;
+		this.id = ThreadLocalRandom.current().nextLong();
 		this.posts = new Stack<RawData>();
 	}
 	
+	/**
+	 * Get the name of the conversation.
+	 * @return the name of the conversation.
+	 */
 	public String getName() {
 		return name;
 	}
 	
-	public int getId() {
+	/**
+	 * Get a unique identifier for the conversation.
+	 * @return a long int identifier
+	 */
+	public long getId() {
 		return id;
 	}
 	
-	@SuppressWarnings("unchecked")
+	/**
+	 * Get a stack containing the posts currently in the conversation.
+	 * @return the posts in raw data form
+	 */
 	public Stack<RawData> getPosts() {
-		return (Stack<RawData>) posts.clone();
+		Stack<RawData> newStack = new Stack<RawData>();
+		newStack.addAll(posts);
+		return newStack;
 	}
 	
+	/**
+	 * Add a new post to the conversation.
+	 * @param postData a RawData object containing the post's data.
+	 */
 	public void post(RawData postData) {
 		posts.add(postData);
 	}
 }
->>>>>>> branch 'init' of https://github.com/dimits-exe/MessagingApp
