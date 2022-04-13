6455e512fbd66dc649829c75c9902908a443ca17
user <user@DESKTOP-S968RHA>
2022-04-13 14:31:33 +0300

    Inconsistent merge state: could not find file MERGE_MSG in .git folder. This file contains the commit message for a merge commit.

--- src/eventDeliverySystem/Message.java
+++ src/eventDeliverySystem/Message.java
@@ -4,15 +4,14 @@ import java.io.Serializable;
 import java.net.InetAddress;
 
 /**
- * An object to be sent between remote system components to
- * describe any change in the state of that system (for example process crashes).
- *
+ * A wrapper holding an object and specifying its type using an enum used for Internet transportation.
+ * Used to facilitate uniform communication between all remote components of the system.
  */
 class Message implements Serializable {
-	private static final long serialVersionUID = 8083153797545703062L;
+	private static final long serialVersionUID = 8083163797545703062L;
 	
 	private final MessageType type;
-	private final InetAddress address;
+	private final Object value;
 	
 	
 	/**
@@ -20,20 +19,42 @@ class Message implements Serializable {
 	 * @param type the type of the message
 	 * @param address the IP address the message is referring to
 	 */
-	public Message(MessageType type, InetAddress address) {
+	public Message(MessageType type, Object value) {
 		this.type = type;
-		this.address = address;
+		this.value = value;
 	}
 
 	public MessageType getType() {
 		return type;
 	}
 
-	public InetAddress getAddress() {
-		return address;
+	public Object getValue() {
+		return value;
 	}
 	
+	/**
+	 * Specifies the purpose and type of payload of the message.
+	 */
 	enum MessageType{
-		REDIRECT, ERROR
+		/**
+		 * Change the connecting IP to a new one.
+		 * The value is a InetAddress object.
+		 */
+		REDIRECT, 
+		/**
+		 * An error has occurred while trying to reach a certain process.
+		 * The value is a InetAddress object.
+		 */
+		ERROR,
+		/**
+		 * A simple data message.
+		 * The value is a Packet object.
+		 */
+		DATA,
+		/**
+		 * A new IP has been connected to the system.
+		 * The value is a InetAddress object.
+		 */
+		DISCOVER
 	}
 }
