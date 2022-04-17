cd5fd87f5c59628c6b6f9c5f25b86cc1634a31d5
Alex Mandelias <alexmandelias@gmail.com>
2022-04-17 12:35:09 +0300

    improve doc and simplify isAvailable

--- src/eventDeliverySystem/PortManager.java
+++ src/eventDeliverySystem/PortManager.java
@@ -5,55 +5,49 @@ import java.net.ServerSocket;
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
- * A class managing and distributing port numbers to the various processes
- * in the system.
- * Used to allow more than one client/server process to run in one machine.
+ * A class that manages and distributes port numbers to the various processes in
+ * this machine. Used to allow more than one client/server process to run in one
+ * machine by providing them with available ports.
  *
+ * @author Dimitris Tsirmpas
  */
 final class PortManager {
+
 	private static final int LOWEST_PORT = 29170;
 	private static final int HIGHEST_PORT = 29998;
-	
+
 	/**
-	 * Get a new port for the process. The port is guaranteed to be available.
-	 * This method is thread-safe.
-	 * 
+	 * Returns a new port for this process in the range {@code 29170-29998} that is
+	 * guaranteed to be available. This method is thread-safe.
+	 *
 	 * @return a new available port
 	 */
 	public synchronized int getPort() {
 		int port;
 		do {
 			port = ThreadLocalRandom.current().nextInt(LOWEST_PORT, HIGHEST_PORT);
-		} while(!isAvailable(port));
-		
+		} while (!PortManager.isAvailable(port));
+
 		return port;
 	}
-	
+
 	/**
-	 * Check whether the port is available.
+	 * Checks whether a port is available.
+	 *
 	 * @param port the port to be checked
-	 * @return true if the port is available, false otherwise
-	 * 
-	 * @implNote the method attempts to create a connection through the port
-	 * and relies on exception handling to determine if it's already binded.
+	 *
+	 * @return {@code true} if the port is available, {@code false} otherwise
+	 *
+	 * @implNote the method attempts to create a connection through the port and
+	 *           relies on exception handling to determine if it's already binded.
 	 */
-	private boolean isAvailable(int port) {
-		ServerSocket testSocket = null;
-	    try {
-	        testSocket = new ServerSocket(port);
+	// https://stackoverflow.com/questions/434718/sockets-discover-port-availability-using-java
+	private static boolean isAvailable(int port) {
+		try (ServerSocket testSocket = new ServerSocket(port)) {
 	        testSocket.setReuseAddress(true);
 	        return true;
 	    } catch (IOException e) {
-	    } finally {
-	        if (testSocket != null) {
-	            try {
-	                testSocket.close();
-	            } catch (IOException e) {
-	                /* should not be thrown */
-	            }
-	        }
+			return false;
 	    }
-
-	    return false;
 	}
 }
