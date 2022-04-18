42d97444cd7e221452670f5815d522c4a7cea2bd
Alex Mandelias <alexmandelias@gmail.com>
2022-04-18 13:46:32 +0300

    implement Packet and small RawData changes

--- src/eventDeliverySystem/Packet.java
+++ src/eventDeliverySystem/Packet.java
@@ -1,115 +1,145 @@
 package eventDeliverySystem;
 
 import java.io.Serializable;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Stream;
 
 import eventDeliverySystem.RawData.DataType;
 
+
 /**
- * A class holding binary data for to-be-transmitted files and text.
- * To be used only in TCP connections.
+ * Provides utility methods to facilitate the transmission of RawData objects by
+ * breaking them into packets and then reassembling them.
+ *
+ * @author Alex Mandelias
+ * @author Dimitris Tsirmpas
  */
 class Packet implements Serializable {
-	private static final int PACKET_SIZE = 521000; // 521kb Chunk size
-	private static final long serialVersionUID = -3948707845170731230L;
-	
+	private static final int  PACKET_SIZE      = (int) (512 * Math.pow(2, 10));
+	private static final long serialVersionUID = 1;
+
 	/**
 	 * Turn a file or text to a series of packets.
-	 * @param data the data of the file
+	 *
+	 * @param data          the data of the file
 	 * @param fileExtension the extension of the file
+	 *
 	 * @return an array of packet objects holding the file's data
 	 */
 	public static Packet[] dataToPackets(RawData data, String fileExtension) {
-		Packet[] packets = new Packet[(int) Math.ceil(data.getData().length/PACKET_SIZE)];
-		//fill the array with packets lol
+		final byte[] src = data.getData();
+
+		final int      packetCount = (int) Math.ceil(src.length / (double) Packet.PACKET_SIZE);
+		final Packet[] packets     = new Packet[packetCount];
+		final long     id          = Packet.getRandomID(); // same for packets of the same RawData
+
+		for (int i = 0; i < packetCount; i++) {
+			final boolean  isFinal = i == (packetCount - 1);
+			final byte[]   payload = new byte[Packet.PACKET_SIZE];
+			final DataType type    = data.getType();
+			final Profile  poster  = data.getPoster();
+
+			if (!isFinal)
+				System.arraycopy(src, i * Packet.PACKET_SIZE, payload, 0, Packet.PACKET_SIZE);
+			else
+				System.arraycopy(src, i * Packet.PACKET_SIZE, payload, 0,
+				        src.length - (i * Packet.PACKET_SIZE));
+
+			final Packet packet = new Packet(id, isFinal, payload, type, fileExtension, poster);
+			packets[i] = packet;
+		}
+
 		return packets;
 	}
-	
+
 	/**
 	 * Turn a string of text to a series of packets.
-	 * @param text the text to be packaged
+	 *
+	 * @param text   the text to be packaged
 	 * @param poster the profile of the user that sent the text
+	 *
 	 * @return an array of packet objects holding the text
 	 */
-	public static Packet[] textToPackets(String username, Profile poster) {
-		return Packet.dataToPackets(new RawData(null, DataType.TEXT, poster), null);
+	public static Packet[] textToPackets(String text, Profile poster) {
+		final RawData rawData = new RawData(text.getBytes(), DataType.TEXT, poster, null);
+		return Packet.dataToPackets(rawData, null);
 	}
-	
+
 	/**
-	 * Extract the data of the packets into a single RawData object.
-	 * @param packets an array with packets holding the entire contents of the file
-	 * @return the packets' contents
-	 * @throws IllegalStateException if there is any error with the contents of the packets
+	 * Extracts the data of some Packets into a single RawData object.
+	 *
+	 * @param packets an array of packets that stores the entire contents of a
+	 *                RawData object
+	 *
+	 * @return the packets' contents as a RawData object
+	 *
+	 * @throws IllegalArgumentException if the packet array has length 0
+	 * @throws IllegalStateException    if packets with different ID are found in
+	 *                                  the array
 	 */
-	public static RawData packetsToData(Packet[] packets) throws IllegalStateException {
-		return null;
+	public static RawData packetsToData(Packet[] packets) {
+		if (packets.length == 0)
+			throw new IllegalArgumentException("Tried to create a RawData object with no data");
+
+		final byte[] data = new byte[Stream.of(packets).mapToInt(p -> p.payload.length).sum()];
+		int          ptr  = 0;
+
+		final long idOfFirst = packets[0].id;
+
+		for (int i = 0, count = packets.length; i < count; i++) {
+			final Packet curr = packets[i];
+
+			if (curr.id != idOfFirst)
+				throw new IllegalStateException(String.format(
+				        "Tried to packet with id %d with packet with id %d", idOfFirst, curr.id));
+
+			final byte[] payload = curr.payload;
+			final int    length  = payload.length;
+			System.arraycopy(payload, 0, data, ptr, length);
+			ptr += length;
+		}
+
+		return new RawData(data, packets[0].type, packets[0].poster,
+		        packets[0].fileExtension);
 	}
-	
-	private final int id;
-	private final boolean isFinal;
-	private final byte[] payload;
-	private final String fileExtension;
-	private final String posterName;
+
+	private static long getRandomID() {
+		return ThreadLocalRandom.current().nextLong();
+	}
+
+	private final long     id;
+	private final boolean  isFinal;
+	private final byte[]   payload;
+	private final String   fileExtension;
+	private final Profile  poster;
 	private final DataType type;
-	
-	private Packet(int id, boolean isFinal, byte[] payload, DataType type, String fileExtension, String posterName) {
+
+	private Packet(long id, boolean isFinal, byte[] payload, DataType type, String fileExtension,
+	        Profile poster) {
 		this.id = id;
 		this.isFinal = isFinal;
 		this.payload = payload;
-		this.posterName = posterName;
+		this.poster = poster;
 		this.type = type;
 		this.fileExtension = fileExtension;
 	}
-	
+
 	/**
-	 * Get a unique identification number of the file chunk contained in the packet.
-	 * @return the id of the file chunk
+	 * Returns this Packet's ID.
+	 *
+	 * @return the ID
 	 */
-	public int getId() {
+	public long getId() {
 		return id;
 	}
-	
+
 	/**
-	 * Whether or not there are more packets for this file on the way.
-	 * @return true if this is the last packet for the file, false otherwise
+	 * Returns whether there are more remaining packets for this RawData object.
+	 *
+	 * @return {@code true} if this is the last packet for the RawData object,
+	 *         {@code false} otherwise
 	 */
 	public boolean isFinal() {
 		return isFinal;
 	}
-	
-	/**
-	 * Returns a copy of the contents of the packet.
-	 * @return a copy of the packet's data
-	 */
-	public byte[] getPayload() {
-		return payload.clone();
-	}
-	
-	/**
-	 * Get the username of the poster.
-	 * @return the username of the poster
-	 */
-	public String getPosterName() {
-		return posterName;
-	}
-	
-	/**
-	 * Get the file extension of the file.
-	 * @return the file extension
-	 * @throws IllegalArgumentException if the contents of the packet are of the DataType "TEXT"
-	 */
-	public String getFileExtension() throws IllegalArgumentException {
-		if(type == DataType.TEXT)
-			throw new IllegalArgumentException("The data of the packet is a string object, not a file.");
-		
-		return fileExtension;
-	}
-
-	/**
-	 * Get the {@link DataType} type of the packet.
-	 * @return the type of the data
-	 */
-	public DataType getType() {
-		return type;
-	}
-	
 }
--- src/eventDeliverySystem/RawData.java
+++ src/eventDeliverySystem/RawData.java
@@ -3,47 +3,84 @@ package eventDeliverySystem;
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
- * An object holding information about a posts's data, its
- * poster and its type.
+ * An object holding information about a posts's data, its poster and its type.
  *
+ * @author Dimitris Tsirmpas
  */
 class RawData {
-	
-	private final Profile poster;
-	private final byte[] data;
-	private final DataType type;
-	private final long postID;
-	
-	public RawData(byte[] data, DataType type, Profile poster) {
-		this.data = data;
-		this.type = type;
-		this.poster = poster;
-		this.postID = ThreadLocalRandom.current().nextLong();
-	}
-	
-	public byte[] getData() {
-		return data.clone();
-	}
-	
-	public DataType getType() {
-		return type;
-	}
-	
-	public Profile getPosterName() {
-		return poster;
-	}
 
-	public Profile getPoster() {
-		return poster;
-	}
-
-	public long getPostID() {
-		return postID;
-	}
-	
 	public enum DataType {
 		TEXT, VIDEO, IMAGE
 	}
 
-}
+	private final byte[]   data;
+	private final DataType type;
+	private final Profile  poster;
+	private final String   fileExtension;
+	private final long     postID;
 
+	/**
+	 * Constructs a new RawData object.
+	 *
+	 * @param data          the data to be encapsulated in the RawData object
+	 * @param type          the Type of the data
+	 * @param poster        the Poster of the data
+	 * @param fileExtension the file extension associated with the data. Must be
+	 *                      {@code null} if {@code type == DataType.TEXT}.
+	 */
+	public RawData(byte[] data, DataType type, Profile poster, String fileExtension) {
+		if ((type == DataType.TEXT) && (fileExtension != null))
+			throw new IllegalArgumentException("DataType TEXT requires 'null' file extension");
+
+		this.data = data;
+		this.type = type;
+		this.poster = poster;
+		this.fileExtension = fileExtension;
+		this.postID = ThreadLocalRandom.current().nextLong();
+	}
+
+	/**
+	 * Returns the poster.
+	 *
+	 * @return the poster
+	 */
+	public Profile getPoster() {
+		return poster;
+	}
+
+	/**
+	 * Returns a clone of the data.
+	 *
+	 * @return the data
+	 */
+	public byte[] getData() {
+		return data.clone();
+	}
+
+	/**
+	 * Returns the type.
+	 *
+	 * @return the type
+	 */
+	public DataType getType() {
+		return type;
+	}
+
+	/**
+	 * Returns the fileExtension.
+	 *
+	 * @return the fileExtension
+	 */
+	public String getFileExtension() {
+		return fileExtension;
+	}
+
+	/**
+	 * Returns the postID.
+	 *
+	 * @return the postID
+	 */
+	public long getPostID() {
+		return postID;
+	}
+}
