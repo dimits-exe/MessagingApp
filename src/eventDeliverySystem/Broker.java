29b3d872fbca8c101473a486e94c01723779fe6a
Alex Mandelias <alexmandelias@gmail.com>
2022-04-19 21:02:31 +0300

    add 2 threads for handling 2 events

--- src/eventDeliverySystem/Broker.java
+++ src/eventDeliverySystem/Broker.java
@@ -1,13 +1,16 @@
 package eventDeliverySystem;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -22,15 +25,23 @@ class Broker implements Runnable {
 	private static final PortManager portManager = new PortManager();
 	private static final int MAX_CONNECTIONS = 64;
 
+	// TODO: this class
+	private final BrokerManager BROKER_MANAGER;
+	// TODO: figure out if this can be final
+	private ConnectionInfo CI;
+
+	// === PLACEHOLDERS ===
+
 	private final Set<Socket> clientConnections; //replace with set<InetAddress>?
 	private final Set<Socket> brokerConnections;
 	private final Map<Topic, LinkedList<Post>> postsPerTopic;
 	private final Map<Topic, LinkedList<Post>> postsBackup;
 
-
 	private ServerSocket clientRequestSocket;
 	private ServerSocket brokerRequestSocket;
 	private boolean isLeader;
+
+	// === END PLACEHOLDERS ===
 
 	/**
 	 * Create a new broker and add it to the already existing
@@ -50,21 +61,94 @@ class Broker implements Runnable {
 	public Broker() {
 		this.clientConnections = new HashSet<>();
 		this.brokerConnections = new HashSet<>();
-		this.postsPerTopic = new HashMap<>();
+		this.postsPerTopic = Collections.synchronizedMap(new HashMap<>());
 		this.postsBackup = new HashMap<>();
 		isLeader = true;
 	}
 
-	/*
-	@foff
-
-	1. basikh roh: stelnei aplo mhnyma ston topic broker
-	2. publisher stelnei mhnyma
-
-
-
-	@fon
+	/**
+	 * A Thread for receiving some data from an input stream.
+	 *
+	 * @author Alex Mandelias
 	 */
+	private class PullThread extends Thread {
+
+		private final ObjectInputStream stream;
+
+		/**
+		 * Constructs the Thread that, when run, will read data from the stream.
+		 *
+		 * @param stream the input stream from which to read the data
+		 */
+		public PullThread(ObjectInputStream stream) {
+			this.stream = stream;
+		}
+
+		@Override
+		public void run() {
+
+			final List<Packet> postFragments = new LinkedList<>();
+
+			try {
+				Packet packet;
+				do {
+					try {
+						packet = (Packet) stream.readObject();
+					} catch (ClassNotFoundException e) {
+						e.printStackTrace();
+						return;
+					}
+					postFragments.add(packet);
+				} while (!packet.isFinal());
+
+				final Packet[] packets = postFragments.toArray(new Packet[postFragments.size()]);
+				final Post     post    = Post.fromPackets(packets);
+				postsPerTopic.get(post.getPostInfo().getTopic()).add(post);
+
+				// TODO: multicast this post to every broker
+
+			} catch (IOException e) {
+				// do nothing
+			}
+		}
+	}
+
+	/**
+	 * A Thread for TODO
+	 *
+	 * @author Alex Mandelias
+	 */
+	private class DiscoveryThread extends Thread {
+
+		private final ObjectOutputStream stream;
+		private final Topic              topic;
+
+		/**
+		 * Constructs the Thread that, when run, will TODO
+		 *
+		 * @param stream  the output stream to which to write the data
+		 * @param message the Message that contains the Topic
+		 */
+		public DiscoveryThread(ObjectOutputStream stream, Message message) {
+			this.stream = stream;
+			this.topic = (Topic) message.getValue();
+		}
+
+		@Override
+		public void run() {
+
+			try {
+
+				Broker broker = BROKER_MANAGER.getBroker(topic.getName());
+				stream.writeObject(broker.CI);
+
+				// TODO: update this' and other broker's ip table with this producer
+
+			} catch (IOException e) {
+				// do nothing
+			}
+		}
+	}
 
 	@Override
 	public void run() {
