12803c67260528968ac25f828372a19782f05fb1
user <user@DESKTOP-S968RHA>
2022-04-21 16:03:11 +0300

    Implement

--- src/eventDeliverySystem/Broker.java
+++ src/eventDeliverySystem/Broker.java
@@ -7,12 +7,15 @@ import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+
+import eventDeliverySystem.Message.MessageType;
 
 /**
  * A remote component that forms the backbone of the EventDeliverySystem.
@@ -24,25 +27,24 @@ class Broker implements Runnable {
 
 	private static final PortManager portManager = new PortManager();
 	private static final int MAX_CONNECTIONS = 64;
+	
 
-	// TODO: this class
-	private final BrokerManager BROKER_MANAGER;
-	// TODO: figure out if this can be final
-	private ConnectionInfo CI;
-
+	private final Set<ConnectionInfo> clientInfo;
+	
 	// === PLACEHOLDERS ===
-
-	private final Set<Socket> clientConnections; //replace with set<InetAddress>?
-	private final Set<Socket> brokerConnections;
+	
+	private final List<Socket> brokerConnections;
 	private final Map<Topic, LinkedList<Post>> postsPerTopic;
 	private final Map<Topic, LinkedList<Post>> postsBackup;
 
-	private ServerSocket clientRequestSocket;
-	private ServerSocket brokerRequestSocket;
 	private boolean isLeader;
 
 	// === END PLACEHOLDERS ===
 
+	private ServerSocket clientRequestSocket;
+	private ServerSocket brokerRequestSocket;
+	
+	
 	/**
 	 * Create a new broker and add it to the already existing
 	 * distributed server system.
@@ -50,7 +52,6 @@ class Broker implements Runnable {
 	 */
 	public Broker(InetAddress leaderIP) {
 		this();
-		subscribeToNewBroker(leaderIP);
 		isLeader = false;
 	}
 
@@ -59,13 +60,174 @@ class Broker implements Runnable {
 	 * distributed server.
 	 */
 	public Broker() {
-		this.clientConnections = new HashSet<>();
-		this.brokerConnections = new HashSet<>();
+		//TODO: establish connection with broker
+		this.clientInfo = Collections.synchronizedSet(new HashSet<>());
+		this.brokerConnections = Collections.synchronizedList(new LinkedList<>());
 		this.postsPerTopic = Collections.synchronizedMap(new HashMap<>());
-		this.postsBackup = new HashMap<>();
+		this.postsBackup = Collections.synchronizedMap(new HashMap<>());
 		isLeader = true;
 	}
 
+	@Override
+	public void run() {
+		try {
+			clientRequestSocket = new ServerSocket(portManager.getNewAvailablePort(), MAX_CONNECTIONS);
+			brokerRequestSocket = new ServerSocket(portManager.getNewAvailablePort(), MAX_CONNECTIONS);
+			
+			// Start handling client requests
+			Runnable clientRequestThread = () -> {
+                while (true) {
+                    try {
+                        Socket socket = clientRequestSocket.accept();
+                        new ClientRequestHandler(socket).start();
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        System.exit(-1); 		// serious error when waiting, close broker
+                    }
+                }
+            }; //clientRequestThread
+            
+            
+            //TODO: properly implement
+			Runnable brokerRequestThread = new Runnable() {
+
+				@Override
+				public void run() {
+					while(true) {
+						try {
+							brokerConnections.add(brokerRequestSocket.accept());
+						} catch (IOException e) {
+							e.printStackTrace();
+							System.exit(-1); 	// serious error when waiting, close broker
+						}
+					}
+				}
+
+			}; //brokerRequestThread
+
+			new Thread(clientRequestThread).start();
+			new Thread(brokerRequestThread).start();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	
+	private Thread threadFactory(Message message, Socket connection) throws IOException {
+		switch(message.getType()) {
+		case DATA_PACKET:
+			PostInfo pi = (PostInfo) message.getValue();
+			return new PullThread(new ObjectInputStream(connection.getInputStream()), pi);
+
+		case DISCOVER:
+			Topic t = (Topic) message.getValue();
+			return new DiscoveryThread(new ObjectOutputStream(connection.getOutputStream()), t);
+			
+		default:
+			throw new IllegalArgumentException("How the fuck");
+		}
+	}
+	
+	// ============== UNUSED =======================
+	
+	/**
+	 * Sends a message to all brokers.
+	 * @param m the messsage
+	 */
+	private void multicastMessage(Message m) {
+		
+		for(Socket connection : brokerConnections) {
+			try(ObjectOutputStream out = new ObjectOutputStream(connection.getOutputStream())) {
+				out.writeObject(m);
+			} catch (IOException ioe) {
+				System.err.println("Message transmission failed: " + ioe.toString());
+			}
+		}
+		
+	}
+	
+	/**
+	 * Return the broker that's responsible for the requested topic.
+	 * @param topic the topic
+	 * @return the {@link ConnectionInfo} of the assigned broker
+	 */
+	private ConnectionInfo getAssignedBroker(Topic topic) {
+		int brokerIndex = topic.hashCode() % brokerConnections.size();
+		final Socket broker = brokerConnections.get(brokerIndex);
+		return new ConnectionInfo(broker.getInetAddress(), broker.getPort());
+	}
+	
+	/**
+	 * Update internal data structures once a new connection has been established with
+	 * another broker.
+	 * 
+	 * @param newBroker the information of the connected broker
+	 */
+	private void newBrokerConnected(Socket newBroker) {
+		if(brokerConnections.contains(newBroker))
+			return;
+		
+		brokerConnections.add(newBroker);
+		brokerConnections.sort(new Comparator<Socket>() {
+			
+			/**
+			 * Compares IP addresses and if equal, with ports.
+			 */
+			@Override
+			public int compare(Socket s1, Socket s2) {
+				
+				int ipc =s1.getInetAddress().getHostName().compareTo(s2.getInetAddress().getHostName());
+				
+				if(ipc == 0) {
+					return s2.getPort() - s1.getPort();
+				} else {
+					return ipc;
+				}
+					
+			}
+		
+		}); //sort
+	}
+	
+	
+	/**
+	 * Get the assigned topics for this broker.
+	 * @return the topics
+	 */
+	private Set<Topic> getAssignedTopics() {
+		return this.postsPerTopic.keySet();
+	}
+	
+
+	 private class ClientRequestHandler extends Thread {
+	
+        private Socket socket;
+
+        public ClientRequestHandler(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+        	
+			try(ObjectInputStream ois 	= new ObjectInputStream(socket.getInputStream())) {
+				Message           m   	= (Message) ois.readObject();
+				Thread thread 			= Broker.this.threadFactory(m, socket);
+				thread.start();
+				
+				Broker.this.clientInfo.add(new ConnectionInfo(socket.getInetAddress(), socket.getPort()));
+			} catch (IOException ioe) {
+				// do nothing
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+
+           
+        }
+    }
+	
 	/**
 	 * A Thread for receiving some data from an input stream.
 	 *
@@ -74,14 +236,16 @@ class Broker implements Runnable {
 	private class PullThread extends Thread {
 
 		private final ObjectInputStream stream;
+		private final PostInfo postInfo;
 
 		/**
 		 * Constructs the Thread that, when run, will read data from the stream.
 		 *
 		 * @param stream the input stream from which to read the data
 		 */
-		public PullThread(ObjectInputStream stream) {
+		public PullThread(ObjectInputStream stream, PostInfo postInfo) {
 			this.stream = stream;
+			this.postInfo = postInfo;
 		}
 
 		@Override
@@ -102,7 +266,7 @@ class Broker implements Runnable {
 				} while (!packet.isFinal());
 
 				final Packet[] packets = postFragments.toArray(new Packet[postFragments.size()]);
-				final Post     post    = Post.fromPackets(packets);
+				final Post     post    = Post.fromPackets(packets, this.postInfo);
 				postsPerTopic.get(post.getPostInfo().getTopic()).add(post);
 
 				// TODO: multicast this post to every broker
@@ -114,7 +278,7 @@ class Broker implements Runnable {
 	}
 
 	/**
-	 * A Thread for TODO
+	 * A Thread for discovering the actual broker for a topic.
 	 *
 	 * @author Alex Mandelias
 	 */
@@ -124,134 +288,29 @@ class Broker implements Runnable {
 		private final Topic              topic;
 
 		/**
-		 * Constructs the Thread that, when run, will TODO
+		 * Constructs the Thread that, when run, will write the address of the broker
+		 * that has the requested topic in the given output stream.
 		 *
 		 * @param stream  the output stream to which to write the data
-		 * @param message the Message that contains the Topic
+		 * @param topic the topic 
 		 */
-		public DiscoveryThread(ObjectOutputStream stream, Message message) {
+		public DiscoveryThread(ObjectOutputStream stream, Topic topic) {
 			this.stream = stream;
-			this.topic = (Topic) message.getValue();
+			this.topic = topic;
 		}
 
 		@Override
 		public void run() {
 
 			try {
-
-				Broker broker = BROKER_MANAGER.getBroker(topic.getName());
-				stream.writeObject(broker.CI);
-
-				// TODO: update this' and other broker's ip table with this producer
-
+				ConnectionInfo brokerInfo = Broker.this.getAssignedBroker(topic);
+				stream.writeObject(brokerInfo);
 			} catch (IOException e) {
 				// do nothing
 			}
 		}
 	}
 
-	@Override
-	public void run() {
-		try {
-			clientRequestSocket = new ServerSocket(portManager.getNewAvailablePort(), MAX_CONNECTIONS);
-			brokerRequestSocket = new ServerSocket(portManager.getNewAvailablePort(), MAX_CONNECTIONS);
 
-			Runnable clientRequestThread = new Runnable() {
-
-				@Override
-				public void run() {
-					while(true) {
-						try {
-							clientConnections.add(clientRequestSocket.accept());
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
-					}
-				}
-
-			}; //clientRequestThread
-
-			Runnable brokerRequestThread = new Runnable() {
-
-				@Override
-				public void run() {
-					while(true) {
-						try {
-							brokerConnections.add(brokerRequestSocket.accept());
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
-					}
-				}
-
-			}; //brokerRequestThread
-
-			new Thread(clientRequestThread).start();
-			new Thread(brokerRequestThread).start();
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	/**
-	 * Add a connection from and to the new broker.
-	 * @param newBrokerIP the IP of the new broker
-	 */
-	private void subscribeToNewBroker(InetAddress newBrokerIP) {
-		try {
-			brokerConnections.add(new Socket(newBrokerIP, portManager.getNewAvailablePort()));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	/**
-	 * Send a {@link Message} redirecting the client to the
-	 * broker assigned to the topic.
-	 *
-	 * @param topic the topic
-	 */
-	private void deferToActualBroker(Topic topic) {
-		if(!isLeader) {
-			throw new IllegalStateException("Non-leader broker asked to redirect to other broker");
-		}
-
-	}
-
-	/**
-	 * Sends a message to all brokers.
-	 * @param m the messsage
-	 */
-	private void multicastMessage(Message m) {
-		try {
-			for(Socket connection : brokerConnections) {
-				ObjectOutputStream out = new ObjectOutputStream(connection.getOutputStream());
-				out.writeObject(m);
-			}
-		} catch (IOException ioe) {
-			System.err.println("Message transmission failed: " + ioe.toString());
-		}
-	}
-
-	/**
-	 * Sends a post to all brokers to be saved remotely.
-	 * @param postPackets the packets of the post
-	 */
-	private void multicastPost(Packet[] postPackets) {
-		try {
-			for(Socket connection : brokerConnections) {
-				ObjectOutputStream out = new ObjectOutputStream(connection.getOutputStream());
-
-				for(Packet p: postPackets) {
-					out.writeObject(p);
-				}
-
-			}
-		} catch (IOException ioe) {
-			System.err.println("Backup failed: " + ioe.toString());
-		}
-	}
-
-
+	
 }
