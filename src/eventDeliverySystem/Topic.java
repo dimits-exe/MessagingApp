6fc1334c1f72541c84f42b853d9ad7bffa513bf8
Alex Mandelias <alexmandelias@gmail.com>
2022-04-22 18:43:06 +0300

    cleanup Topic

--- src/eventDeliverySystem/Topic.java
+++ src/eventDeliverySystem/Topic.java
@@ -1,34 +1,34 @@
 package eventDeliverySystem;
 
+import java.security.DigestException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.EmptyStackException;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Stack;
-import java.util.concurrent.ThreadLocalRandom;
-import java.security.DigestException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 /**
  * A data structure holding the contents of a conversation/topic between users.
  * Note that it does NOT keep track of users subscribed to it.
- * @author user
  *
+ * @author Alex Mandelias
+ * @author Dimitris Tsirmpas
  */
 class Topic {
+	private static final int special = -1;
+
 	private final String name;
-	private final long id;
 	private final Stack<Post> posts;
 
 	/**
-	 * Create a new conversation.
-	 * @param name the name displayed when viewing the conversation.
+	 * Creates a new empty Topic.
+	 *
+	 * @param name the Topic's unique name.
 	 */
 	public Topic(String name) {
-		super();
 		this.name = name;
-		this.id = ThreadLocalRandom.current().nextLong();
 		this.posts = new Stack<>();
 	}
 
@@ -41,38 +41,6 @@ class Topic {
 	}
 
 	/**
-	 * Get a unique identifier for the conversation.
-	 * @return a long int identifier
-	 */
-	public long getId() {
-		return id;
-	}
-
-	/**
-	 * Get a list containing posts posted after the provided postId.
-	 * @param lastPostId the id of the last post saved in the conversation
-	 * @return the new posts sorted last-to-first posted
-	 * @throws NoSuchElementException if the lastPostId doesn't correspond to
-	 * a post in the topic
-	 */
-	public List<Post> getPostsSince(long lastPostId) throws NoSuchElementException {
-		Stack<Post> newStack = new Stack<>();
-		newStack.addAll(posts);
-
-		LinkedList<Post> newPosts = new LinkedList<>();
-		try {
-
-			do {
-				newPosts.add(newStack.pop());
-			} while (newStack.peek().getPostInfo().getId() != lastPostId);
-
-		} catch(EmptyStackException e) {
-			throw new NoSuchElementException("There is no post with this idlol wtf"); //maybe just send all posts anyway
-		}
-		return newPosts;
-	}
-
-	/**
 	 * Add a new post to the conversation.
 	 *
 	 * @param postData a Post object containing the post's data.
@@ -81,19 +49,46 @@ class Topic {
 		posts.add(postData);
 	}
 
+	/**
+	 * Get a list containing posts posted after the provided postId.
+	 *
+	 * @param lastPostId the id of the last post saved in the conversation
+	 *
+	 * @return the new posts sorted last-to-first posted
+	 *
+	 * @throws NoSuchElementException if the lastPostId doesn't correspond to a post
+	 *                                in the topic
+	 */
+	public List<Post> getPostsSince(long lastPostId) throws NoSuchElementException {
+
+		Stack<Post> postsClone = new Stack<>();
+		postsClone.addAll(posts);
+
+		if (lastPostId == Topic.special) {
+			return new LinkedList<>(postsClone);
+		}
+
+		LinkedList<Post> newPosts = new LinkedList<>();
+		try {
+
+			do {
+				newPosts.add(postsClone.pop());
+			} while (postsClone.peek().getPostInfo().getId() != lastPostId);
+
+		} catch(EmptyStackException e) {
+			throw new NoSuchElementException("There is no post with this idlol wtf");
+		}
+
+		return newPosts;
+	}
+
 	@Override
 	public int hashCode() {
-		int hash = 0;
 		try {
-			hash = MessageDigest.getInstance("md5").digest(name.getBytes(), 0, 0);
-		} catch (DigestException e) {
-			e.printStackTrace();
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
+			return MessageDigest.getInstance("md5").digest(name.getBytes(), 0, 0);
+		} catch (DigestException | NoSuchAlgorithmException e) {
+			throw new RuntimeException(e);
 		}
-		
-		// if this actually returns 0 I dont fucking care
-		return hash;
 	}
 
 	@Override
