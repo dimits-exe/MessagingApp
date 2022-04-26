ee9117c417b5e778c5ba1816b74b08803ebf5492
user <user@DESKTOP-S968RHA>
2022-04-26 19:58:24 +0300

    Create User class interface, implement testing in new class

--- src/eventDeliverySystem/User.java
+++ src/eventDeliverySystem/User.java
@@ -1,133 +1,108 @@
 package eventDeliverySystem;
 
-import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
-import java.util.HashSet;
+import java.net.InetAddress;
+import java.nio.file.Path;
 import java.util.List;
-import java.util.Scanner;
-import java.util.Set;
 
-import javax.imageio.ImageIO;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.WindowConstants;
-
-import eventDeliverySystem.Post.DataType;
-
-@SuppressWarnings("javadoc")
+	
+/**
+ * A class that manages the actions of the user by communicating with the server
+ * and retrieving / committing posts to the file system.
+ * 
+ * @author Alex Mandelias
+ * @author Dimitris Tsirmpas
+ */
 public class User {
-
-	public User() {
-		// TODO Auto-generated constructor stub
+	
+	private final Publisher publisher;
+	private final Consumer consumer;
+	private final Path userDir;
+	private final Profile user;
+	
+	/**
+	 * Retrieve the user's data and the saved posts, establish connection to the server
+	 * and prepare to receive and send posts.
+	 * @param serverIP the IP of the server
+	 * @param port the port of the server
+	 * @param userDir the base directory of all the saved posts and topics
+	 * @throws IOException if a connection can't be established with the server or
+	 * if the file system can't be reached
+	 */
+	public User(InetAddress serverIP, int port, Path userDir) throws IOException {
+		this.userDir = userDir;
+		
+		// load user data
+		try {
+			this.user = loadUserData();
+		} catch(IOException ioe) {
+			throw new IOException("Could not load user data.", ioe);
+		}
+		
+		// create user processes
+		try {
+			this.publisher = new Publisher(serverIP, port);
+			this.consumer = new Consumer(serverIP, port, user.getSubscribedTopics());
+		} catch(IOException ioe) {
+			throw new IOException("Could not establish connection with server.", ioe);
+		}
+		
+	}
+	
+	/**
+	 * Commit all new posts to the File System.
+	 * @param topicName the name of the topic
+	 * @throws IOException if the connection with the server is interrupted 
+	 * or if the File System is unreachable
+	 */
+	public void pull(String topicName) throws IOException {
+		List<Post> posts = consumer.pull(topicName);
+		
+		for(Post post : posts) {
+			savePost(post);
+		}
+	}
+	
+	/**
+	 * Request the remote server to create a new topic with the specified name.
+	 * @param topicName the name of the new topic
+	 */
+	public void createTopic(String TopicName) {
+		publisher.createTopic(TopicName);
+	}
+	
+	/**
+	 * Send a new post to the server.
+	 * @param post the new post
+	 * @throws IOException if the connection with the server is interrupted
+	 */
+	public void post(Post post) throws IOException {
+		publisher.push(post);
+		savePost(post);
+	}
+	
+	/**
+	 * Get the directory used to save the application's posts and data.
+	 * @return the user directory
+	 */
+	public Path getUserDirectory() {
+		return userDir;
+	}
+	
+	/**
+	 * Commit a new Post to the File System.
+	 * @param post the post to be saved
+	 * @throws IOException if the File System is unreachable
+	 */
+	private void savePost(Post post) throws IOException {
+		//TODO: implement
+	}
+	
+	
+	
+	private Profile loadUserData() throws IOException {
+		//TODO: implement
+		return null;
 	}
 
-	public static void main(String[] args)
-	        throws NumberFormatException, IOException {
-		int port = 29863;
-
-		Profile poster1 = new Profile("alex");
-		sout("poster");
-
-		Publisher p = new Publisher("192.168.1.145", port);
-		sout("publisher");
-
-		Set<Topic> t = new HashSet<>(List.of(new Topic("opa")));
-		sout("topic");
-
-		// Consumer c = new Consumer("192.168.1.145", port, t);
-		// sout("consumer");
-
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		BufferedImage         img  = ImageIO
-		        .read(new File("C:\\users\\alexm\\Desktop\\epic-cat2.png"));
-		ImageIO.write(img, "jpg", baos);
-		byte[] bytes = baos.toByteArray();
-		Post   post1 = new Post(bytes, DataType.IMAGE, poster1.getId(), ".png", "opa");
-		sout("img");
-
-		p.push(post1);
-		sout("post");
-
-		Consumer c = new Consumer("192.168.1.145", port, t);
-		sout("consumer");
-		Scanner scanner = new Scanner(System.in);
-		scanner.nextLine();
-		List<Post> posts = c.pull("opa");
-		Post       cat   = posts.get(0);
-
-		sout("pull");
-
-		byte[]               mpitia = cat.getData();
-		ByteArrayInputStream bais   = new ByteArrayInputStream(mpitia);
-		BufferedImage        image  = ImageIO.read(bais);
-		Image                img1   = image.getScaledInstance(800, 600, 0);
-		JFrame frame = new JFrame();
-		JPanel panel = new JPanel() {
-							@Override
-							public void paintComponent(Graphics g) {
-								super.paintComponent(g);
-											g.drawImage(img1, 0, 0, null);
-							}
-						};
-
-		frame.setSize(800, 600);
-		frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-		frame.setVisible(true);
-		frame.add(panel);
-		sout("end");
-
-
-
-
-
-
-		/*
-		@foff
-		baos = new ByteArrayOutputStream();
-		img = ImageIO.read(new File("C:\\users\\alexm\\Desktop\\epic-cat2.png"));
-		ImageIO.write(img, "jpg", baos);
-		bytes = baos.toByteArray();
-		post1 = new Post(bytes, DataType.IMAGE, poster1.getId(), ".png", "opa");
-		p.push(post1);
-		sout("img");
-
-		sout("consumer");
-		scanner.nextLine();
-		posts = c.pull("opa");
-		sout("%d", posts.size());
-		cat = posts.get(1);
-
-		sout("pull");
-
-		mpitia = cat.getData();
-		bais = new ByteArrayInputStream(mpitia);
-		image = ImageIO.read(bais);
-		Image  img2   = image.getScaledInstance(800, 600, 0);
-		JFrame frame1 = new JFrame();
-		JPanel panel1 = new JPanel() {
-			@Override
-			public void paintComponent(Graphics g) {
-				super.paintComponent(g);
-								g.drawImage(img2, 0, 0, null);
-			}
-		};
-
-		frame1.setSize(800, 600);
-		frame1.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		frame1.setVisible(true);
-		frame1.add(panel1);
-		sout("end");
-		@fon
-		*/
-	}
-
-	private static void sout(String format, Object... args) {
-		System.out.printf(format + "\n", args);
-		System.out.flush();
-	}
 }
