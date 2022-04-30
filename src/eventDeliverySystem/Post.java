a7d0cd2cecd7fd66c2fe68e80a5347df46d2cd1a
Alex Mandelias <alexmandelias@gmail.com>
2022-04-30 15:13:49 +0300

    fix Post constructors

--- src/eventDeliverySystem/Post.java
+++ src/eventDeliverySystem/Post.java
@@ -1,5 +1,9 @@
 package eventDeliverySystem;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Stream;
 
@@ -13,102 +17,126 @@ import java.util.stream.Stream;
  */
 class Post {
 
-	public enum DataType {
-		TEXT, VIDEO, IMAGE
-	}
-
 	/**
-	 * Extracts the data of some Packets into a single RawData object.
+	 * Extracts the data of some Packets into a single Post.
 	 *
-	 * @param packets an array of packets that stores the entire contents of a
-	 *                RawData object
+	 * @param packets  an array of packets that stores the data of a Post
+	 * @param postInfo the PostInfo object associated with that Post
 	 *
-	 * @return the packets' contents as a RawData object
+	 * @return the packets' contents as a Post
 	 *
-	 * @throws IllegalArgumentException if the packet array has length 0
-	 * @throws IllegalStateException    if packets with different ID are found in
-	 *                                  the array
+	 * @throws IllegalArgumentException if the packets array has length 0
+	 * @throws IllegalStateException    if Packets with different IDs are found in
+	 *                                  the packets array
 	 */
 	public static Post fromPackets(Packet[] packets, PostInfo postInfo) {
 		if (packets.length == 0)
-			throw new IllegalArgumentException("Tried to create a RawData object with no data");
+			throw new IllegalArgumentException("Tried to create a Post object with no data");
 
-		final byte[] data = new byte[Stream.of(packets).mapToInt(p -> p.getPayload().length).sum()];
-		int          ptr  = 0;
+		final int    byteCount = Stream.of(packets).mapToInt(p -> p.getPayload().length).sum();
+		final byte[] data      = new byte[byteCount];
 
 		final long idOfFirst = packets[0].getPostId();
 
-		for (int i = 0, count = packets.length; i < count; i++) {
+		int dataPointer = 0;
+		for (int i = 0, packetCount = packets.length; i < packetCount; i++) {
 			final Packet curr = packets[i];
 
 			if (curr.getPostId() != idOfFirst)
 				throw new IllegalStateException(String.format(
-				        "Tried to combine packet with id %d with packet with id %d", idOfFirst,
+				        "Tried to combine packet with ID %d with packet with ID %d", idOfFirst,
 				        curr.getPostId()));
 
 			final byte[] payload = curr.getPayload();
 			final int    length  = payload.length;
-			System.arraycopy(payload, 0, data, (ptr += length) - length, length);
+			System.arraycopy(payload, 0, data, dataPointer, length);
+			dataPointer += length;
 		}
 
 		return new Post(data, postInfo);
+	}
+
+	/**
+	 * Constructs a Post with from a File.
+	 *
+	 * @param file     the File whose data will be encapsulated in a Post
+	 * @param posterId the ID of the poster of the File
+	 *
+	 * @return the Post that encapsulates the contents of the File
+	 *
+	 * @throws FileNotFoundException if the File could not be found
+	 * @throws IOException           if an I/O Error occurs
+	 */
+	public static Post fromFile(File file, long posterId)
+	        throws FileNotFoundException, IOException {
+
+		byte[] data;
+		try (FileInputStream fis = new FileInputStream(file)) {
+			data = fis.readAllBytes();
+		}
+
+		final String fileName      = file.getName();
+		final String fileExtension = fileName.substring(fileName.lastIndexOf('.') + 1);
+		return new Post(data, posterId, fileExtension);
+	}
+
+	/**
+	 * Constructs a Post from a String of text, a plain-text message.
+	 *
+	 * @param text     the message
+	 * @param posterId the ID of the poster of the plain-message
+	 *
+	 * @return the Post that encapsulates the plain-text message
+	 */
+	public static Post fromText(String text, long posterId) {
+		return new Post(text.getBytes(), posterId, "~txt");
 	}
 
 	private final byte[]   data;
 	private final PostInfo postInfo;
 
 	/**
-	 * Constructs a new Post with a random ID.
+	 * Constructs a new Post with the specified info.
 	 *
-	 * @param data          the data to be encapsulated in the Post
-	 * @param type          the Type of the data of the Post
-	 * @param posterId      the ID of the Poster of the Post
-	 * @param fileExtension the file extension associated with the data of the Post.
-	 *                      Must be {@code null} if {@code type == DataType.TEXT}.
-	 * @param topicName     the Topic of the Post
+	 * @param data     the contents of this post
+	 * @param postInfo the PostInfo object associated with this Post
 	 */
-	public Post(byte[] data, DataType type, long posterId, String fileExtension,
-	        String topicName) {
-		this(data, type, posterId, fileExtension, topicName,
-		        ThreadLocalRandom.current().nextLong());
+	Post(byte[] data, PostInfo postInfo) {
+		this.data = data;
+		this.postInfo = postInfo;
 	}
 
 	/**
-	 * Constructs a new Post with the specified info.
+	 * Constructs a new Post with a random ID.
 	 *
-	 * @param data the contents of the post
-	 * @param postInfo the info of the post
+	 * @param data          the data to be encapsulated in this Post
+	 * @param posterId      the ID of the poster of this Post
+	 * @param fileExtension the file extension associated with the data of this
+	 *                      Post. Plain-text messages have a file extension of
+	 *                      '{@code ~txt}'
 	 */
-	public Post(byte[] data, PostInfo postInfo) {
-		this(data, postInfo.getType(), postInfo.getPosterId(), postInfo.getFileExtension(),
-		        postInfo.getTopicName(),
-				postInfo.getId());
+	private Post(byte[] data, long posterId, String fileExtension) {
+		this(data, posterId, fileExtension, ThreadLocalRandom.current().nextLong());
 	}
 
 	/**
 	 * Constructs a new Post with a set ID.
 	 *
-	 * @param data          the data to be encapsulated in the Post
-	 * @param type          the Type of the data of the Post
-	 * @param posterId      the ID of the Poster of the Post
-	 * @param fileExtension the file extension associated with the data of the Post.
-	 *                      Must be {@code null} if {@code type == DataType.TEXT}.
-	 * @param topicName     the Topic of the Post
-	 * @param postID        the id of the Post
+	 * @param data          the data to be encapsulated in this Post
+	 * @param posterId      the ID of the poster of this Post
+	 * @param fileExtension the file extension associated with the data of this
+	 *                      Post. Plain-text messages have a file extension of
+	 *                      '{@code ~txt}'
+	 * @param postID        the ID of this Post
 	 */
-	private Post(byte[] data, DataType type, long posterId, String fileExtension, String topicName,
-	        long postID) {
-		if ((type == DataType.TEXT) && (fileExtension != null))
-			throw new IllegalArgumentException("DataType TEXT requires 'null' file extension");
-
-		this.data = data;
-		this.postInfo = new PostInfo(type, posterId, fileExtension, topicName, postID);
+	private Post(byte[] data, long posterId, String fileExtension, long postID) {
+		this(data, new PostInfo(posterId, fileExtension, postID));
 	}
 
 	/**
-	 * Returns a clone of the data.
+	 * Returns a clone of this Post's data.
 	 *
-	 * @return the data
+	 * @return a clone of the data
 	 */
 	public byte[] getData() {
 		return data.clone();
