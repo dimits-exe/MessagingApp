0b1f8cbdeaf2d906a0f6681fde60e059242f5aec
Alex Mandelias <alexmandelias@gmail.com>
2022-04-30 15:23:07 +0300

    improve code readability in Profile

--- src/eventDeliverySystem/Profile.java
+++ src/eventDeliverySystem/Profile.java
@@ -1,108 +1,144 @@
 package eventDeliverySystem;
 
 import java.util.Arrays;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.NoSuchElementException;
-import java.util.Objects;
-import java.util.Set;
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
- * A data structure holding information about a user and
- * his subscribed topics.
+ * A data structure holding information about a user and their subscribed
+ * topics.
  *
+ * @author Alex Mandelias
+ * @author Dimitris Tsirmpas
  */
 class Profile {
 
-	private final String     name;
-	private final long       id;
-	private final Set<Topic> subscribedTopics;
+	private final String             name;
+	private final long               id;
+	private final Map<String, Topic> topics;
 
 	/**
-	 * Create a new user profile with the specified name.
-	 * @param name the name of the user
+	 * Creates a new, empty, Profile with the specified name and a random unique ID.
+	 * This constructor should be used when creating a new Profile, since the ID is
+	 * not known and is randomly generated.
+	 *
+	 * @param name the name of the Profile
 	 */
 	public Profile(String name) {
-		this.name = name;
-		this.id = ThreadLocalRandom.current().nextLong();
-		this.subscribedTopics = new HashSet<Topic>();
+		this(name, ThreadLocalRandom.current().nextLong(), new HashMap<>());
 	}
 
 	/**
-	 * Create a new user profile with the specified name and subscribed topics.
-	 * @param name the username
-	 * @param subscribedTopics a set of already subscribed topics
+	 * Creates a new, empty, Profile with the specified name and unique ID. This
+	 * constructor should be used when loading a saved Profile from disk, since the
+	 * ID is known.
+	 *
+	 * @param name the name of the Profile
+	 * @param id   the Profile's unique ID
 	 */
-	public Profile(String name, Set<Topic> subscribedTopics) {
+	public Profile(String name, long id) {
+		this(name, id, new HashMap<>());
+	}
+
+	private Profile(String name, long id, Map<String, Topic> topics) {
 		this.name = name;
-		this.id = ThreadLocalRandom.current().nextLong();
-		this.subscribedTopics = subscribedTopics;
+		this.id = id;
+		this.topics = topics;
 	}
 
 	/**
-	 * Get the username of the profile.
-	 * @return the name of the user
+	 * Returns this Profile's name.
+	 *
+	 * @return the name
 	 */
 	public String getName() {
 		return name;
 	}
 
 	/**
-	 * Get a unique id of the user.
-	 * @return the user's id
+	 * Returns this Profile's id.
+	 *
+	 * @return the id
 	 */
 	public long getId() {
 		return id;
 	}
 
 	/**
-	 * Get all the subscribed topics.
-	 * @return a copy of all the subscribed topics
+	 * Returns this Profile's topics.
+	 *
+	 * @return the topics
 	 */
-	public Set<Topic> getSubscribedTopics() {
-		HashSet<Topic> copyTopics = new HashSet<Topic>();
-		copyTopics.addAll(subscribedTopics);
-		return copyTopics;
+	public Map<String, Topic> getTopics() {
+		// TODO: should this return a copy (a deep copy)?
+		// In this case we need to have a Topic(Topic) copy-constructor
+		return topics;
 	}
 
 	/**
-	 * Unsubscribe the user from a topic.
-	 * @param topic the topic
-	 * @throws NoSuchElementException if the user wasn't subscribed to the topic
+	 * Adds a new, unique, Topic to this Profile.
+	 *
+	 * @param topicName the name of the new Topic
+	 *
+	 * @throws IllegalArgumentException if a Topic with the same name already exists
 	 */
-	public void unsubscribeFromTopic(Topic topic) throws NoSuchElementException {
-		subscribedTopics.remove(topic);
+	public void addTopic(String topicName) {
+		addTopic(new Topic(topicName));
 	}
 
 	/**
-	 * Subscribe to a new topic
-	 * @param topic the topic
+	 * Adds a new Topic to this Profile.
+	 *
+	 * @param topic the Topic
+	 *
+	 * @throws NullPointerException     if {@code topic == null}
+	 * @throws IllegalArgumentException if a Topic with the same name already exists
 	 */
-	public void subscribeToTopic(Topic topic) {
-		subscribedTopics.add(topic);
+	public void addTopic(Topic topic) {
+		if (topic == null)
+			throw new NullPointerException("Topic can't be null");
+
+		final String topicName = topic.getName();
+		if (topics.containsKey(topic.getName()))
+			throw new IllegalArgumentException("Topic with name " + topicName + " already exists");
+
+		topics.put(topicName, topic);
 	}
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
+	/**
+	 * Updates a Topic of this Profile with new Posts.
+	 *
+	 * @param topicName the name of the Topic to update
+	 * @param posts     the new Posts to post to the Topic
+	 *
+	 * @throws NoSuchElementException if no Topic with the given name exists
+	 */
+	public void updateTopic(String topicName, List<Post> posts) {
+		final Topic topic = topics.get(topicName);
+		if (topic == null)
+			throw new NoSuchElementException("No Topic with name " + topicName + " found");
+
+		topic.post(posts);
 	}
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Profile other = (Profile) obj;
-		return id == other.id;
+	/**
+	 * Removes a Topic from this Profile.
+	 *
+	 * @param topicName the name of the Topic
+	 *
+	 * @throws NoSuchElementException if no Topic with the given name exists
+	 */
+	public void removeTopic(String topicName) throws NoSuchElementException {
+		if (topics.remove(topicName) == null)
+			throw new NoSuchElementException("No Topic with name " + topicName + " found");
 	}
 
 	@Override
 	public String toString() {
-		List<Object> words = Arrays.asList(subscribedTopics.stream().map(t -> t.getName()).toArray());
-		return String.format("Profile [name=%s, id=%d, subscribedTopics=%s]", name, id, words);
+		final List<Object> topicNames = Arrays.asList(topics.keySet().toArray());
+		return String.format("Profile [name=%s, id=%d, topics=%s]", name, id, topicNames);
 	}
 }
