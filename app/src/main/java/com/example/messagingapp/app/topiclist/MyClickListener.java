0267fa1e6c010cbfdb72232de4c51f9b07d224d2
Alex Mandelias <alexmandelias@gmail.com>
2022-06-11 19:58:02 +0300

    adds doc and TODOs for VideoViewHolder

--- app/src/main/java/com/example/messagingapp/app/topiclist/MyClickListener.java
+++ app/src/main/java/com/example/messagingapp/app/topiclist/MyClickListener.java
@@ -1,7 +1,19 @@
 package com.example.messagingapp.app.topiclist;
 
-public interface MyClickListener {
+/**
+ * Allows the TopicListActivity to provide a callback for the TopicListAdapter when the user clicks
+ * on an item of the Adapter, when the user wants to view the contents of a conversation.
+ *
+ * @author Alex Mandelias
+ */
+interface MyClickListener {
 
+    /**
+     * Should be called when the user clicks on a conversation. This should start a TopicActivity
+     * which displays that conversation.
+     *
+     * @param topicName the name of the Topic to display
+     */
     void onClick(String topicName);
 
 }
