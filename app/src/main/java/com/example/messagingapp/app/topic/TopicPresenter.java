d751ee213bb9c8e9ef09b8b89a3e1b4f8b9dfee6
dimits-exe <manisfatsa@gmail.com>
2022-06-11 16:41:04 +0300

    Finish topic UI implementation

--- app/src/main/java/com/example/messagingapp/app/topic/TopicPresenter.java
+++ app/src/main/java/com/example/messagingapp/app/topic/TopicPresenter.java
@@ -1,5 +1,6 @@
 package com.example.messagingapp.app.topic;
 
+import android.net.Uri;
 import android.util.Log;
 
 import com.example.messagingapp.app.util.strategies.IErrorMessageStrategy;
@@ -15,16 +16,37 @@ import java.io.IOException;
  */
 class TopicPresenter {
     private final IErrorMessageStrategy errorMessageStrategy;
+    private final File baseDir;
     private final User user;
     private final String topicName;
 
-    public TopicPresenter(IErrorMessageStrategy errorMessageStrategy, User user, String topicName) {
+    public TopicPresenter(IErrorMessageStrategy errorMessageStrategy, File baseDir, User user, String topicName) {
         this.errorMessageStrategy = errorMessageStrategy;
+        this.baseDir = baseDir;
         this.user = user;
         this.topicName = topicName;
     }
 
-    public void trySendFile(File file) {
+    public File getTempFileDir(){
+        return new File(baseDir, "temp_file");
+    }
+
+    public void sendFile(Uri fileUri) {
+        if(fileUri != null){
+            trySendFile(new File(fileUri.getPath()));
+
+            boolean success = getTempFileDir().delete();
+            if(!success)
+                Log.e("Topic", "Temp file not deleted");
+        }
+    }
+
+    public void sendText(String text) {
+        //TODO: implement
+        Log.i("Placeholder", "Message " + text + " sent");
+    }
+
+    private void trySendFile(File file) {
         if(file == null)
             return;
 
@@ -36,22 +58,9 @@ class TopicPresenter {
         }
     }
 
-    public void sendText(String text) {
-        //TODO: implement
-    }
-
-    public void takeAndSendPhoto() {
-        File photo = takePhoto();
-        trySendFile(photo);
-    }
-
     private void sendFile(File file) throws IOException {
         //TODO: implement
-    }
-
-    private File takePhoto() {
-        //TODO: implement
-        return null;
+        Log.i("Placeholder", "File " + file.toString() + " sent");
     }
 
 }
