d6cc30753117a9a87a37ba04abd6e672136e15e5
Alex Mandelias <alexmandelias@gmail.com>
2022-06-11 17:34:13 +0300

    adds check for port number

--- app/src/main/java/com/example/messagingapp/app/connect/ConnectActivity.java
+++ app/src/main/java/com/example/messagingapp/app/connect/ConnectActivity.java
@@ -16,63 +16,57 @@ import java.util.Objects;
  * @author Dimitris Tsirmpas
  */
 public class ConnectActivity extends AppCompatActivity {
+
     private final ConnectPresenter presenter = new ConnectPresenter();
-    private TextInputLayout ipEditText, portEditText;
+
+    private TextInputLayout ipTextInputLayout, portTextInputLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect);
 
-        ipEditText = findViewById(R.id.connection_server_IP_input);
-        portEditText = findViewById(R.id.connection_server_port_input);
+        ipTextInputLayout = findViewById(R.id.connection_server_IP_input);
+        portTextInputLayout = findViewById(R.id.connection_server_port_input);
 
         findViewById(R.id.connect_button).setOnClickListener(e -> onSubmit());
     }
 
-    /**
-     * Parse user provided arguments, check them, if correct launch login activity.
-     */
+    /** Validates the IP and Port and launches the Login Activity if they are correct */
     public void onSubmit() {
-        //clear previous error messages
-        ipEditText.setError(null);
-        portEditText.setError(null);
+        // clear previous error messages
+        ipTextInputLayout.setError(null);
+        portTextInputLayout.setError(null);
 
-        //extract user provided data
-        String serverIP = Objects.requireNonNull(ipEditText.getEditText()).getText().toString();
-        String serverPort = Objects.requireNonNull(portEditText.getEditText()).getText().toString();
+        String serverIP = Objects.requireNonNull(ipTextInputLayout.getEditText()).getText().toString();
+        String serverPort = Objects.requireNonNull(portTextInputLayout.getEditText()).getText().toString();
 
-        if(!checkParams(serverIP, serverPort)) {
+        if (!checkParams(serverIP, serverPort)) {
             return;
         }
 
-        int port = Integer.parseInt(serverPort);
+        Integer port = Integer.valueOf(serverPort);
 
         System.out.printf("Server IP: %s Server Port: %d%n", serverIP, port);
-        toNextActivity(serverIP, port);
-    }
-
-    private boolean checkParams(String serverIP, String serverPort) {
-        boolean success = true;
-        if(!presenter.checkIP(serverIP)){
-            success = false;
-            ipEditText.setError("Wrong IP format");
-        }
-
-        if(!presenter.checkPort(serverPort)){
-            success = false;
-            portEditText.setError(serverPort + " is not a valid number");
-        }
-
-        return success;
-    }
-
-    private void toNextActivity(String serverIP, int serverPort){
         Intent intent = new Intent(this, LoginActivity.class);
         intent.putExtra(LoginActivity.ARG_IP, serverIP);
-        intent.putExtra(LoginActivity.ARG_PORT, serverPort);
+        intent.putExtra(LoginActivity.ARG_PORT, port);
 
         startActivity(intent);
     }
 
+    private boolean checkParams(String serverIP, String serverPort) {
+        boolean success = true;
+        if (!presenter.checkIP(serverIP)) {
+            success = false;
+            ipTextInputLayout.setError(serverIP + " is not a valid IPv4 address");
+        }
+
+        if (!presenter.checkPort(serverPort)) {
+            success = false;
+            portTextInputLayout.setError(serverPort + " is not a valid port number");
+        }
+
+        return success;
+    }
 }
--- app/src/main/java/com/example/messagingapp/app/connect/ConnectPresenter.java
+++ app/src/main/java/com/example/messagingapp/app/connect/ConnectPresenter.java
@@ -3,34 +3,38 @@ package com.example.messagingapp.app.connect;
 import java.util.regex.Pattern;
 
 /**
- * A presenter handling the checking of the connect form's fields.
+ * A presenter handling the validation of the connect form's fields.
  *
+ * @author Alex Mandelias
  * @author Dimitris Tsirmpas
  */
 class ConnectPresenter {
+
     private static final Pattern IP_PATTERN = Pattern.compile(
             "^(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\\.(?!$)|$)){4}$");
 
+    private static final Pattern PORT_PATTERN = Pattern.compile(
+            "^([1-9][0-9]{0,3}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$");
+
     /**
-     * Check if the provided IP is a syntactically correct IPv4 address.
-     * @param providedIP the user provided IP
-     * @return true if the IP is valid, false otherwise
+     * Returns whether the given IP address is a syntactically correct IPv4 address.
+     *
+     * @param ip the IP to check
+     *
+     * @return {@code true} if the IP is valid, {@code false} otherwise
      */
-    public boolean checkIP(String providedIP) {
-        return IP_PATTERN.matcher(providedIP).matches();
+    public boolean checkIP(String ip) {
+        return IP_PATTERN.matcher(ip).matches();
     }
 
     /**
-     * Check if the provided port is a valid integer.
-     * @param providedPort the user provided port
-     * @return true if the port is a valid integer, false otherwise
+     * Returns whether the given port number is a valid port number.
+     *
+     * @param port the port number to check
+     *
+     * @return {@code true} if the port number is a valid port number, {@code false} otherwise
      */
-    public boolean checkPort(String providedPort){
-        try {
-            int result = Integer.parseInt(providedPort);
-            return result > 0;
-        } catch (NumberFormatException ne){
-            return false;
-        }
+    public boolean checkPort(String port) {
+        return PORT_PATTERN.matcher(port).matches();
     }
 }
