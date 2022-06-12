4d9d450119df61123ff1b48531be469cda98df06
dimits-exe <manisfatsa@gmail.com>
2022-06-12 12:26:55 +0300

    oxi :sunflower:

--- app/src/main/java/com/example/messagingapp/app/connect/ConnectPresenter.java
+++ app/src/main/java/com/example/messagingapp/app/connect/ConnectPresenter.java
@@ -5,16 +5,14 @@ import java.util.regex.Pattern;
 /**
  * A presenter handling the validation of the connect form's fields.
  *
- * @author Alex Mandelias
  * @author Dimitris Tsirmpas
  */
 class ConnectPresenter {
 
     private static final Pattern IP_PATTERN = Pattern.compile(
             "^(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\\.(?!$)|$)){4}$");
-
-    private static final Pattern PORT_PATTERN = Pattern.compile(
-            "^([1-9][0-9]{0,3}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$");
+    private static final int PORT_LOWER_LIMIT = 0;
+    private static final int PORT_UPPER_LIMIT = 65535;
 
     /**
      * Returns whether the given IP address is a syntactically correct IPv4 address.
@@ -35,6 +33,11 @@ class ConnectPresenter {
      * @return {@code true} if the port number is a valid port number, {@code false} otherwise
      */
     public boolean checkPort(String port) {
-        return PORT_PATTERN.matcher(port).matches();
+        try {
+            int portNumber = Integer.parseInt(port);
+            return portNumber >= PORT_LOWER_LIMIT && portNumber <= PORT_UPPER_LIMIT;
+        } catch (NumberFormatException ne) {
+            return false;
+        }
     }
 }
