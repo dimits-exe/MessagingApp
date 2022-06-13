12a037589525d5b11d39883c8c8124fcc5beb8fe
dimits-exe <manisfatsa@gmail.com>
2022-06-13 13:46:10 +0300

    Connect with backend

--- app/src/main/java/com/example/messagingapp/app/topic/TopicPresenter.java
+++ app/src/main/java/com/example/messagingapp/app/topic/TopicPresenter.java
@@ -27,6 +27,13 @@ class TopicPresenter {
 
     private File tempFile = null;
 
+    /**
+     * Create a new TopicPresenter.
+     * @param errorMessageStrategy the strategy with which error messages are shown to the user
+     * @param baseDir the base file directory of the application
+     * @param user the logged-in user instance
+     * @param topicName the name of the current topic
+     */
     public TopicPresenter(IErrorMessageStrategy errorMessageStrategy, File baseDir, User user, String topicName) {
         this.errorMessageStrategy = errorMessageStrategy;
         this.baseDir = baseDir;
@@ -52,6 +59,12 @@ class TopicPresenter {
         return user.getCurrentProfile().getTopic(topicName).getAllPosts();
     }
 
+    /**
+     * Get a temporary file. The file will be deleted automatically on the next method call or
+     * on VM exit.
+     *
+     * @return a new temporary file
+     */
     public synchronized File getTempFileDir(){
         if(tempFile != null){
             try {
@@ -71,36 +84,54 @@ class TopicPresenter {
         return tempFile;
     }
 
+    /**
+     * Send a file to the topic. Displays an error to the user if any error occurs.
+     * @param fileUri the uri of the file
+     */
     public void sendFile(Uri fileUri) {
         if(fileUri != null){
-            trySendFile(new File(fileUri.getPath()));
-
-            boolean success = getTempFileDir().delete();
-            if(!success)
-                Log.e(TAG, "Temp file not deleted");
+            try {
+                Post post = Post.fromFile(new File(fileUri.getPath()), user.getCurrentProfile().getName());
+                trySendPost(post);
+            } catch (IOException e) {
+                errorMessageStrategy.showError("An error occured while sending the file");
+                Log.e(TAG, "Send file", e);
+            }
+            
         }
     }
 
+    /**
+     * Send a text message to the topic. Displays an error to the user if any error occurs.
+     * @param text the text message
+     */
     public void sendText(String text) {
-        //TODO: implement
+        Post post = Post.fromText(text, user.getCurrentProfile().getName());
+        trySendPost(post);
         Log.i(TAG, "Message " + text + " sent");
     }
 
-    private void trySendFile(File file) {
-        if(file == null)
-            return;
-
+    /**
+     * Send a {@link Post}, displaying an error to the user if any error occurs.
+     * @param post the post to be sent
+     */
+    private void trySendPost(Post post) {
         try {
-            sendFile(file);
+            sendFile(post);
         } catch (IOException ioe) {
             errorMessageStrategy.showError("Error on sending file");
             Log.e(TAG, "send file", ioe);
         }
     }
 
-    private void sendFile(File file) throws IOException {
-        //TODO: implement
-        Log.i(TAG,"File " + file.toString() + " sent");
+    /**
+     * Send a {@link Post} to the remote topic.
+     * @param post the post to be sent
+     * @throws IOException if any error occurs while transmitting the post
+     */
+    private void sendFile(Post post) throws IOException {
+        user.post(post, topicName);
+        Log.i(TAG,"File " + post.toString() + " sent");
     }
 
 }
